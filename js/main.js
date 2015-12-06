08776a2eed79fe2e3f949ec8b5c17880fb6f4566
rustem <m.rustem18@gmail.com>
2015-12-06 22:34:24 +0300

    added clients switch

--- js/main.js
+++ js/main.js
@@ -51,28 +51,47 @@ $(document).ready(function() {
   $("#request-form").submit(function(e) {
     
               
-    $('#submit-btn').html('Подождите...');
-    var url = "send_mail.php"; // the script where you handle the form input.
+      $('#submit-btn').html('Подождите...');
+      var url = "send_mail.php"; // the script where you handle the form input.
+      
+      $.ajax({
+            type: "POST",
+            url: url,
+            data: $("#request-form").serialize(), // serializes the form's elements.
+            success: function(data)
+            {
+                $('#request-form').hide();
+                $('#main-form').css('margin-top', "-150px");
+                
+                $('#final-step').show();
+                
+                alignCenter();
+                $('#submit-btn').html('Отправить<span class="arrow"">></span>');
+            }
+          });
+  
+      e.preventDefault(); // avoid to execute the actual submit of the form.
+  });
     
-    $.ajax({
-           type: "POST",
-           url: url,
-           data: $("#request-form").serialize(), // serializes the form's elements.
-           success: function(data)
-           {
-              $('#request-form').hide();
-              $('#main-form').css('margin-top', "-150px");
-              
-              $('#final-step').show();
-              
-              alignCenter();
-              $('#submit-btn').html('Отправить<span class="arrow"">></span>');
-           }
-         });
-
-    e.preventDefault(); // avoid to execute the actual submit of the form.
-});
+  $('#clients .previous').on('click', function() {
+    $('.clients-logos img.show').first().hide( "fast", function() {
+      $(this).removeClass('show').addClass('hide').appendTo('.clients-logos');
+    });
     
+    $('.clients-logos img.hide').first().show( "fast", function() {
+      $(this).removeClass('hide').addClass('show');
+    });
+  });
+  
+  $('#clients .next').on('click', function() {
+    $('.clients-logos img.show').last().hide( "fast", function() {
+      $(this).removeClass('show').addClass('hide').appendTo('.clients-logos');
+    });
+    
+    $('.clients-logos img.hide').first().prependTo('.clients-logos').show( "fast", function() {
+      $(this).removeClass('hide').addClass('show');
+    });
+  });
   
 });
 
